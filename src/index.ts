e8d3434d37d90b22de1c3954b90f66901d7ee5d1
gerrit:jdreyer <j.dreyer@fh-muenster.de>
2023-01-28 12:54:11 +0100

    reset switch with timeout

--- src/index.ts
+++ src/index.ts
@@ -8,197 +8,257 @@ import {
   Logging,
   PlatformAccessory,
   PlatformAccessoryEvent,
-  PlatformConfig
-} from 'homebridge';
-const python = require('node-calls-python').interpreter; // eslint-disable-line @typescript-eslint/no-var-requires
-import process from 'child_process';
-import { DeviceConfig, RfSwitchPlatformConfig } from './configTypes';
+  PlatformConfig,
+} from 'homebridge'
+const python = require('node-calls-python').interpreter // eslint-disable-line @typescript-eslint/no-var-requires
+import process from 'child_process'
+import { DeviceConfig, RfSwitchPlatformConfig } from './configTypes'
 
-let hap: HAP;
-let Accessory: typeof PlatformAccessory;
+let hap: HAP
+let Accessory: typeof PlatformAccessory
 
-const PLUGIN_NAME = 'homebridge-rpi-rf-switch';
-const PLATFORM_NAME = 'rfSwitch';
+const PLUGIN_NAME = 'homebridge-rpi-rf-switch'
+const PLATFORM_NAME = 'rfSwitch'
 
 type Command = {
-  accessory: PlatformAccessory,
-  state: CharacteristicValue,
+  accessory: PlatformAccessory
+  state: CharacteristicValue
   callback: CharacteristicSetCallback
-};
+}
 
 class RfSwitchPlatform implements DynamicPlatformPlugin {
-  private readonly log: Logging;
-  private readonly api: API;
-  private readonly config: RfSwitchPlatformConfig;
-  private readonly accessories: Array<PlatformAccessory>;
-  private readonly commandQueue: Array<Command>;
-  private readonly rfDevice: any; // eslint-disable-line @typescript-eslint/no-explicit-any
-  private transmitting: boolean;
+  private readonly log: Logging
+  private readonly api: API
+  private readonly config: RfSwitchPlatformConfig
+  private readonly accessories: Array<PlatformAccessory>
+  private readonly commandQueue: Array<Command>
+  private readonly rfDevice: any // eslint-disable-line @typescript-eslint/no-explicit-any
+  private readonly resetTime: number
+  private transmitting: boolean
 
   constructor(log: Logging, config: PlatformConfig, api: API) {
-    this.log = log;
-    this.config = config as RfSwitchPlatformConfig;
-    this.api = api;
+    this.log = log
+    this.config = config as RfSwitchPlatformConfig
+    this.api = api
 
-    this.accessories = [];
-    this.commandQueue = [];
-    this.transmitting = false;
+    this.accessories = []
+    this.commandQueue = []
+    this.transmitting = false
 
-    let libpython = config.libpython;
+    let libpython = config.libpython
 
     if (!libpython) {
-      let pyconfig = process.execSync('python3-config --libs').toString();
-      let index = pyconfig.indexOf('-lpython');
-      pyconfig = pyconfig.substr(index + 2);
-      index = pyconfig.indexOf(' ');
-      libpython = pyconfig.substr(0, index);
+      let pyconfig = process.execSync('python3-config --libs').toString()
+      let index = pyconfig.indexOf('-lpython')
+      pyconfig = pyconfig.substr(index + 2)
+      index = pyconfig.indexOf(' ')
+      libpython = pyconfig.substr(0, index)
     }
 
-    python.fixlink('lib' + libpython + '.so');
+    python.fixlink('lib' + libpython + '.so')
 
-    const gpio = config.gpio || 17;
-    const repeat = config.repeat || 10;
-    const resetTime = config.resetSwitchTime || 0;
+    const gpio = config.gpio || 17
+    const repeat = config.repeat || 10
+    this.resetTime = config.resetSwitchTime || 0
 
-    const rpiRf = python.importSync('rpi_rf');
+    const rpiRf = python.importSync('rpi_rf')
     try {
-      this.rfDevice = python.createSync(rpiRf, 'RFDevice', gpio, 1, null, repeat, 24);
+      this.rfDevice = python.createSync(
+        rpiRf,
+        'RFDevice',
+        gpio,
+        1,
+        null,
+        repeat,
+        24
+      )
     } catch (ex) {
-      this.log.error('Error starting rpi-rf, please make sure you\'ve followed the ' +
-        'installation instructions in this project\'s readme: ' + ex);
-      return;
+      this.log.error(
+        "Error starting rpi-rf, please make sure you've followed the " +
+          "installation instructions in this project's readme: " +
+          ex
+      )
+      return
     }
-    python.callSync(this.rfDevice, 'enable_tx');
+    python.callSync(this.rfDevice, 'enable_tx')
 
-    api.on(APIEvent.DID_FINISH_LAUNCHING, this.didFinishLaunching.bind(this));
+    api.on(APIEvent.DID_FINISH_LAUNCHING, this.didFinishLaunching.bind(this))
   }
 
   configureAccessory(accessory: PlatformAccessory): void {
-    this.setService(accessory);
-    this.accessories.push(accessory);
+    this.setService(accessory)
+    this.accessories.push(accessory)
   }
 
   didFinishLaunching(): void {
-    const serials: Array<string> = [];
+    const serials: Array<string> = []
     if (this.config.devices) {
       this.config.devices.forEach((device: DeviceConfig) => {
         if (!device.name) {
-          this.log.error('One of your devices has no name configured. This instance will be skipped.');
+          this.log.error(
+            'One of your devices has no name configured. This instance will be skipped.'
+          )
         } else if (!device.on_code) {
-          this.log.error('One of your devices has no on_code configured. This instance will be skipped.');
+          this.log.error(
+            'One of your devices has no on_code configured. This instance will be skipped.'
+          )
         } else if (!device.off_code) {
-          this.log.error('One of your devices has no off_code configured. This instance will be skipped.');
+          this.log.error(
+            'One of your devices has no off_code configured. This instance will be skipped.'
+          )
         } else {
-          this.addAccessory(device);
-          serials.push(device.on_code + ':' + device.off_code);
+          this.addAccessory(device)
+          serials.push(device.on_code + ':' + device.off_code)
         }
-      });
+      })
     }
 
-    const badAccessories = this.accessories.filter((cachedAccessory: PlatformAccessory) => {
-      return !serials.includes(cachedAccessory.context.serial);
-    });
-    this.removeAccessories(badAccessories);
+    const badAccessories = this.accessories.filter(
+      (cachedAccessory: PlatformAccessory) => {
+        return !serials.includes(cachedAccessory.context.serial)
+      }
+    )
+    this.removeAccessories(badAccessories)
   }
 
   addAccessory(data: DeviceConfig): void {
-    this.log('Initializing platform accessory \'' + data.name + '\'...');
-    const serial = data.on_code + ':' + data.off_code;
+    this.log("Initializing platform accessory '" + data.name + "'...")
+    const serial = data.on_code + ':' + data.off_code
 
-    let accessory = this.accessories.find(cachedAccessory => {
-      return cachedAccessory.context.serial == serial;
-    });
+    let accessory = this.accessories.find((cachedAccessory) => {
+      return cachedAccessory.context.serial == serial
+    })
 
     if (!accessory) {
-      const uuid = hap.uuid.generate(serial);
-      accessory = new Accessory(data.name!, uuid);
+      const uuid = hap.uuid.generate(serial)
+      accessory = new Accessory(data.name!, uuid)
 
-      accessory.addService(hap.Service.Switch, data.name);
+      accessory.addService(hap.Service.Switch, data.name)
 
-      this.setService(accessory);
+      this.setService(accessory)
 
-      this.api.registerPlatformAccessories('homebridge-rpi-rf-switch', 'rfSwitch', [accessory]);
+      this.api.registerPlatformAccessories(
+        'homebridge-rpi-rf-switch',
+        'rfSwitch',
+        [accessory]
+      )
 
-      this.accessories.push(accessory);
+      this.accessories.push(accessory)
     }
 
-    accessory.context = data;
-    accessory.context.serial = serial;
+    accessory.context = data
+    accessory.context.serial = serial
 
-    const accInfo = accessory.getService(hap.Service.AccessoryInformation);
+    const accInfo = accessory.getService(hap.Service.AccessoryInformation)
     if (accInfo) {
       accInfo
         .setCharacteristic(hap.Characteristic.Manufacturer, 'Sunoo')
         .setCharacteristic(hap.Characteristic.Model, 'rpi-rf')
-        .setCharacteristic(hap.Characteristic.SerialNumber, accessory.context.serial);
+        .setCharacteristic(
+          hap.Characteristic.SerialNumber,
+          accessory.context.serial
+        )
     }
   }
 
   removeAccessories(accessories: Array<PlatformAccessory>): void {
     accessories.forEach((accessory: PlatformAccessory) => {
-      this.log(accessory.context.name + ' is removed from Homebridge.');
-      this.api.unregisterPlatformAccessories('homebridge-rpi-rf-switch', 'rfSwitch', [accessory]);
-      this.accessories.splice(this.accessories.indexOf(accessory), 1);
-    });
+      this.log(accessory.context.name + ' is removed from Homebridge.')
+      this.api.unregisterPlatformAccessories(
+        'homebridge-rpi-rf-switch',
+        'rfSwitch',
+        [accessory]
+      )
+      this.accessories.splice(this.accessories.indexOf(accessory), 1)
+    })
   }
 
   setService(accessory: PlatformAccessory): void {
-    const service = accessory.getService(hap.Service.Switch);
+    const service = accessory.getService(hap.Service.Switch)
     if (service) {
       service
         .getCharacteristic(hap.Characteristic.On)
-        .on('set', this.setPowerState.bind(this, accessory));
+        .on('set', this.setPowerState.bind(this, accessory))
+      if (this.resetTime > 0) {
+        setTimeout(
+          function () {
+            service.setCharacteristic(hap.Characteristic.On, false)
+          }.bind(this),
+          this.resetTime
+        )
+      }
     }
 
     accessory.on(PlatformAccessoryEvent.IDENTIFY, () => {
-      this.log(accessory.displayName, 'identify requested!');
-    });
+      this.log(accessory.displayName, 'identify requested!')
+    })
   }
 
-  setPowerState(accessory: PlatformAccessory, state: CharacteristicValue, callback: CharacteristicSetCallback): void {
+  setPowerState(
+    accessory: PlatformAccessory,
+    state: CharacteristicValue,
+    callback: CharacteristicSetCallback
+  ): void {
     this.commandQueue.push({
-      'accessory': accessory,
-      'state': state,
-      'callback': callback
-    });
+      accessory: accessory,
+      state: state,
+      callback: callback,
+    })
     if (!this.transmitting) {
-      this.transmitting = true;
-      this.nextCommand.bind(this)();
+      this.transmitting = true
+      this.nextCommand.bind(this)()
     }
   }
 
   nextCommand(): void {
-    const todoItem = this.commandQueue.shift();
+    const todoItem = this.commandQueue.shift()
     if (!todoItem) {
-      return;
+      return
     }
 
-    const code = todoItem.state ? todoItem.accessory.context.on_code : todoItem.accessory.context.off_code;
+    const code = todoItem.state
+      ? todoItem.accessory.context.on_code
+      : todoItem.accessory.context.off_code
 
-    python.call(this.rfDevice, 'tx_code', code, todoItem.accessory.context.protocol,
-      todoItem.accessory.context.pulselength, todoItem.accessory.context.codelength)
+    python
+      .call(
+        this.rfDevice,
+        'tx_code',
+        code,
+        todoItem.accessory.context.protocol,
+        todoItem.accessory.context.pulselength,
+        todoItem.accessory.context.codelength
+      )
       .then((): void => {
-        this.log.debug(todoItem.accessory.context.name + ' is turned ' + (todoItem.state ? 'on.' : 'off.'));
-        todoItem.accessory.context.state = todoItem.state;
+        this.log.debug(
+          todoItem.accessory.context.name +
+            ' is turned ' +
+            (todoItem.state ? 'on.' : 'off.')
+        )
+        todoItem.accessory.context.state = todoItem.state
 
         if (this.commandQueue.length > 0) {
-          this.nextCommand.bind(this)();
+          this.nextCommand.bind(this)()
         } else {
-          this.transmitting = false;
+          this.transmitting = false
         }
 
-        todoItem.callback();
+        todoItem.callback()
       })
       .catch((error: Error) => {
-        this.log('Failed to turn ' + (todoItem.state ? 'on ' : 'off ') + todoItem.accessory.context.name);
-        this.log(error.message);
-      });
+        this.log(
+          'Failed to turn ' +
+            (todoItem.state ? 'on ' : 'off ') +
+            todoItem.accessory.context.name
+        )
+        this.log(error.message)
+      })
   }
 }
 
 export = (api: API): void => {
-  hap = api.hap;
-  Accessory = api.platformAccessory;
+  hap = api.hap
+  Accessory = api.platformAccessory
 
-  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, RfSwitchPlatform);
-};
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, RfSwitchPlatform)
+}
